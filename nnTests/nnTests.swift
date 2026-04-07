f7aaf25e0bb5538e3c48fa2b42fb6560865f8e19
GWAN <116233013+KGWAN@users.noreply.github.com>
2026-04-07 14:20:34 +0900

    diary_nn_test

---
+++ nnTests/nnTests.swift
@@ -0,0 +1,17 @@
+//
+//  nnTests.swift
+//  nnTests
+//
+//  Created by JUNGGWAN KIM on 7/23/25.
+//
+
+import Testing
+@testable import nn
+
+struct nnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
